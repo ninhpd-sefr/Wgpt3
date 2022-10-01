3bb4eed696da99cffede36732c3769299f97092b
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2022-10-01 21:47:44 +0700

    update1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Article,Brand,CTA, Navbar} from './components'
+import {Footer,Blog,Possibility,Features,WhatGPT3,Header} from './containers'
+import './App.css'
+function App(props) {
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
